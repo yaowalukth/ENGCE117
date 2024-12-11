62477709a125db86fa516ea35e0b6c8d516bb10d
yaowalukth <16917yaowaluk@gmail.com>
2024-12-11 22:46:26 +0700

    Create HW1-3.cpp

---
+++ HW1/HW1-3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char FN[10] ;
+    char LN[10] ;
+    printf( "First Name :" ) ;
+    scanf( "%s",FN) ;
+    printf( "Last Name :" ) ;
+    scanf( "%s",LN) ;
+    printf( "%s %s TC, RMUTL, Chiang mai, Thailand" , FN , LN ) ;
+    return 0 ;
+}
