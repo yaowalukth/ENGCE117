55e1fb6bc3f770298de1aa764b5a68d05978b649
yaowalukth <16917yaowaluk@gmail.com>
2024-12-15 15:23:11 +0700

    Create HW04-1.cpp

---
+++ HW4/HW04-1.cpp
@@ -0,0 +1,16 @@
+/*
+    จงแสดงผลตัวแปรทั้งหมดเรียงลำดับดังนี้ a b c d
+
+    Output:
+        10 20.0 Haha a
+*/
+#include <stdio.h>
+
+int main() {
+    int a = 10 ;
+    float b = 20 ;
+    char c[] = "Haha" ;
+    char d = 'a' ;
+    printf( "%d %.1f %s %c" , a , b , c , d ) ; //TODO: #10 Display variable a, b, c and d.
+    return 0 ;
+}//end function
