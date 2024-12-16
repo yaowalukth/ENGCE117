27d41b82fd18cfe8430a3ba95da14f5b4ccaabcd
yaowalukth <16917yaowaluk@gmail.com>
2024-12-17 00:00:39 +0700

    Create HW04-4.cpp

---
+++ HW4/HW04-4.cpp
@@ -0,0 +1,25 @@
+/*
+    เขียนโปรแกรมเพื่อคำนวณหาพื้นที่สามเหลี่ยม เมื่อผู้ใช้กรอกค่าได้เพียง 2 ค่าคือ ส่วนสูง และ ฐาน เช่น ส่วนสูง 6 และฐานคือ 2 ดังนั้นพื้นที่สามเหลี่ยมคือ (6x2)/2 = 6
+    
+    Test case:
+        Enter H & W :
+            6 2
+    Output:
+        Triagle Area : 6.0
+
+    Test case:
+        Enter H & W :
+            3 5
+    Output:
+        Triagle Area : 7.5
+*/
+
+#include <stdio.h>
+
+int main() {
+    float H , W ;
+    printf( "Enter H & W :" ) ;
+    scanf( "%f %f", &H , &W ) ;
+    printf( "Triagle Area : %.1f", (H*W)/2 ) ;
+    return 0 ;
+}
