cd3e52df2213034a353e5a1c9aa07d15a2be4a26
yaowalukth <16917yaowaluk@gmail.com>
2025-03-04 23:01:05 +0700

    Update HW08-4.cpp

--- HW8/HW08-4.cpp
+++ HW8/HW08-4.cpp
@@ -1,1 +1,25 @@
-
+#include <stdio.h>
+int main() {
+    int num ;
+    int answer = 1 ;
+    printf( "Input number :" ) ;
+    scanf( "%d", &num ) ;
+    if( num < 1 ) {
+        printf( "Factorial is not defined for negative numbers.\n" ) ;
+        return 0;
+    } else {
+        for( int i = num ; i >= 1 ; i -- ) {
+            answer *= i ;
+        }//end for loop
+        printf( "%d! = ", num ) ;
+        for( int i = num ; i >= 1 ; i -- ) {
+            printf( "%d", i ) ;
+            if( i != 1 ) {
+                printf( "x" ) ;
+            }//end if
+        }//end for loop
+        printf( "\n" ) ;
+        printf( "%d! = %d",num , answer ) ;
+    }//end if else
+    return 0 ;
+}//end function
